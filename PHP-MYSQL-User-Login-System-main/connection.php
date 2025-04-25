5b56976c7e3ce81cab471ce36ef928dcae7352b5
Srashti11 <109288289+Srashti11@users.noreply.github.com>
2025-04-25 15:58:52 +0530

    Update connection.php
    update connection.php with rds credentials

--- PHP-MYSQL-User-Login-System-main/connection.php
+++ PHP-MYSQL-User-Login-System-main/connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$server = "localhost";
-$username = "root";
-$password = "";
+$server = "cloudphp-staging-rds.cexw0qmgqir0.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "Admin$123";
 $db = "login";
 
 $conn = new mysqli($server, $username, $password, $db);
