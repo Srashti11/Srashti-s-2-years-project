648c8fa64757a696a6dda308e828fae87ea736ab
Srashti11 <109288289+Srashti11@users.noreply.github.com>
2025-04-27 01:30:58 +0530

    Update connection.php

--- PHP-MYSQL-User-Login-System-main/connection.php
+++ PHP-MYSQL-User-Login-System-main/connection.php
@@ -3,7 +3,7 @@
 $server = "cloudphp-staging-rds.cexw0qmgqir0.us-east-1.rds.amazonaws.com";
 $username = "admin";
 $password = "Admin$123";
-$db = "loudphpstagingdb";
+$db = "cloudphpstagingdb";
 
 $conn = new mysqli($server, $username, $password, $db);
 
